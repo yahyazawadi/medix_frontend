ffd926afdda1782da53a442a40c112eec028e46a
yahyazawadi <yahyazawadi278@gmail.com>
2024-08-06 19:32:25 +0300

    tried new app.js

--- src/App.js
+++ src/App.js
@@ -1,65 +1,57 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import { useGetDataQuery } from './app/api/apiSlice';
 import TopBar from './components/common/TopBar';
 import AppNavbar from './components/common/NavigationBar';
-import ContactPage from './components/ContactUsPage/ContactPage'; 
+import ContactPage from './components/ContactUsPage/ContactPage';
 import HomePage from './components/Home/HomePage';
 import ServicesPage from './components/ServicesPage/ServicesPage';
-//import Order from "./components/TaxiRequest/TaxiRequest";
-import Login from "./components/Login/Login";
-import RegisterForm from "./components/CreateProfile/CreateProfile";
-//import AdminPanel from "./components/admincontrolpanel/AdminPanel";
-//import Drivers from './components/admincontrolpanel/Drivers';
-//import TaxiRides from './components/admincontrolpanel/TaxiRides';
-import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
-import ResetPassword from "./components/ResetPassword/ResetPassword";
-//import Dashboard from "./components/admincontrolpanel/Dashboard";
+import Login from './components/Login/Login';
+import RegisterForm from './components/CreateProfile/CreateProfile';
+import ForgotPassword from './components/ForgotPassword/ForgotPassword';
+import ResetPassword from './components/ResetPassword/ResetPassword';
 import Reports from './components/admincontrolpanel/Registries';
-import Customers from "./components/admincontrolpanel/Users";
+import Customers from './components/admincontrolpanel/Users';
 import Footer from './components/common/Footer';
 import AdminLinks from './components/admincontrolpanel/adminlinks';
 import UserLinks from './components/UserLink/UserLink';
 import Courses from './components/Courses/Courses';
 import Green from './components/Green/Green';
 
-function App() {
-  const [data, setData] = useState([]);
+const App = () => {
+  const { data, error, isLoading } = useGetDataQuery();
 
-  useEffect(() => {
-    fetch('https://medix-backend-k0q1.onrender.com')
-      .then((response) => response.json())
-      .then((data) => setData(data))
-      .catch((error) => console.error('Error fetching data:', error));
-  }, []);
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
   return (
-    <Router>
-      <div>
-        <TopBar />
-        <AppNavbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          
-          
-          <Route path="/services" element={<ServicesPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/create-profile" element={<RegisterForm />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="/contact" element={<ContactPage />} />
-
-         
-          <Route path="/registries" element={<Reports />} />
-          <Route path="/users" element={<Customers />} />
-          <Route path="/adminpanel/adminlinks" element={<AdminLinks />} />
-          <Route path="/userlink" element={<UserLinks />} />
-          <Route path="/courses" element={<Courses />} />
-          <Route path="/green" element={<Green />} />
-        </Routes>
-       
-      </div>
-    </Router>
-    
+    <Provider store={store}>
+      <Router>
+        <div>
+          <TopBar />
+          <AppNavbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/services" element={<ServicesPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/create-profile" element={<RegisterForm />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/reset-password" element={<ResetPassword />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/registries" element={<Reports />} />
+            <Route path="/users" element={<Customers />} />
+            <Route path="/adminpanel/adminlinks" element={<AdminLinks />} />
+            <Route path="/userlink" element={<UserLinks />} />
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/green" element={<Green />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </Provider>
   );
-}
+};
 
 export default App;
--- src/app/store.js
+++ src/app/store.js
@@ -1,16 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit"
-import { apiSlice } from './api/apiSlice'
-import { setupListeners } from "@reduxjs/toolkit/query"
-import authReducer from '../features/auth/authSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import { apiSlice } from './api/apiSlice';
 
 export const store = configureStore({
-    reducer: {
-        [apiSlice.reducerPath]: apiSlice.reducer,
-        auth: authReducer,
-    },
-    middleware: getDefaultMiddleware =>
-        getDefaultMiddleware().concat(apiSlice.middleware),
-    devTools: false
-})
-
-setupListeners(store.dispatch)
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
+});
