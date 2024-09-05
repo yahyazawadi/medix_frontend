aab8793ec73c1ecc1aa532c5ea207890cd38344e
yahyazawadi <yahyazawadi278@gmail.com>
2024-09-05 20:57:31 +0300

    added texts

--- src/components/Home/Masthead/Masthead.js
+++ src/components/Home/Masthead/Masthead.js
@@ -5,10 +5,10 @@ import Background from '../../BackGround/BackGround';
 function Masthead() {
   return (
     <Background>
-    <header className="masthead vh-100">
+    <div className="masthead">
       <div className='welcome'>welcome to ClimaMedix</div>
-       
-    </header>
+       <div className='healthcare'>Healthcare Teams United For Creating a Sustainable and Greener Future</div></div>
+    <div className='challenge'>We are dedicated to engaging young healthcare professionals in addressing one of the greatest challenges our planet faces—climate change. The well-being of humanity is intricately linked to the health of the Earth. By empowering them, we ensure they play a crucial role in tackling this global crisis. </div>
     </Background>
   );
 }
