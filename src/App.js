23c0be616334c7374aed9e17272938a64edfe794
yahyazawadi <yahyazawadi278@gmail.com>
2024-09-09 21:24:11 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ const App = () => {
           <Route path="/green" element={<Green />} />
           <Route path="/data-fetcher" element={<DataFetcher />} /> {/* Add the new route */}
         </Routes>
-        
+        <Footer />
       </div>
     </Router>
   );
--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import './footer.css';
 
 function Footer() {
   return (
-    <footer className="bg-dark text-white text-center py-3">
-      <div className="container">
-        <p>Call Us 24/7 123-456-7890</p>
-        <p>Follow us on social media</p>
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-section about-us">
+          <h4 className="footer-title">About us</h4>
+          <div className="social-icons">
+            <a href="#"><img src={require('../../assets/facebook.png')} alt="Facebook" className="logon" /></a>
+            <a href="#"><img src={require('../../assets/insta.png')} alt="Instagram" className="logon" /></a>
+          </div>
+        </div>
+
+        <div className="footer-section links">
+          <h4 className="footer-title">hyperlink</h4>
+          <ul className="footer-list">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Courses</a></li>
+            <li><a href="#">Programs</a></li>
+            <li><a href="#">Medix plus</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        <h6>&copy; ClimaMedix. All rights reserved</h6>
       </div>
     </footer>
   );
